c3499a11dfaf1495a3d4c550a47aaffb652be471
aniTech-addict <kirti7666anand15@gmail.com>
2025-12-24 13:01:57 +0530

    refactor(extension): optimize problem name extraction logic and clean up debug logs
    - Restructure getProblemName to use early return for non-LeetCode URLs
    - Comment out console.log statements in sendMessage and content script
    - Add targetSection reference in content script for future use

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 console.log("Background script loaded");
 
 function sendMessage(tabId, message, options){
-    console.log("Attempting to send message to tab", tabId, ":", message);
+    // console.log("Attempting to send message to tab", tabId, ":", message);
     chrome.tabs.sendMessage(tabId, message, options)
-        .then(response => {
-            console.log("Message sent successfully to tab", tabId, "response:", response);
-        })
+        // .then(response => {
+        //     console.log("Message sent successfully to tab", tabId, "response:", response);
+        // })
         .catch(error => {
             console.error("Failed to send message to tab", tabId, ":", error);
         });
 }
+
 function getProblemName(tab) {
-    if (tab.url && tab.url.includes("leetcode.com/problems/")) {
-        const urlParts = tab.url.split("/");
-        const problemName = urlParts[4];
-        console.log("Extracted problem name:", problemName, "for tab", tab.id);
-        sendMessage(tab.id, {type: "problemName", data: problemName});
-        
-    } else {
+    if (!(tab.url && tab.url.includes("leetcode.com/problems/"))) {
         console.log("Tab URL does not match LeetCode problem page:", tab.url);
+        return;
     }
+    const urlParts = tab.url.split("/");
+    const problemName = urlParts[4];
+    console.log("Extracted problem name:", problemName, "for tab", tab.id);
+    sendMessage(tab.id, {type: "problemName", data: problemName});
 }
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
@@ -29,6 +29,7 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // takes quite a time to send message through this listener
     if (changeInfo.status === 'complete') {
         getProblemName(tab);
     }
--- content_script.js
+++ content_script.js
@@ -1,10 +1,12 @@
 console.log("Content script loaded on LeetCode problem page");
 (() => {
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        console.log("Message received in content script:", message);
+        // console.log("Message received in content script:", message);
         if(message.type === "problemName"){
             console.log(message.data);
         }
         sendResponse({status: "success", messageType: message.type});
     });
 })()
+
+const targetSection = document.getElementById("code_tabbar_outer");
